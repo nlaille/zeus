7630c59651a7249aaee34624beb38a2b1222d9e4
Nicolas Laille <laille.nicolas@gmail.com>
2015-03-14 17:49:05 +0100

    Ajout de kafka-node

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var kafka = require('kafka-node');
+
+var client = new kafka.Client();
+var producer = new kafka.Producer(client);
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.json({});
+  	res.json({});
+});
+
+/* POST home page. */
+router.post('/', function(req, res) {
+	var payloads = [
+        { topic: 'test', messages: [req.body.message] }
+    ];
+	producer.send(payloads, function (err, data) {
+    	console.log(data);
+	});
+  	res.json({});
 });
 
 module.exports = router;
